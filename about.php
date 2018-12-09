56d1d5e345fe5049e2933f9ee7317ce22deeeb15
maaax128 <44034742+maaax128@users.noreply.github.com>
2018-12-09 22:06:51 +0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html><html lang="ru">
+  <head>
+     <meta charset="UTF-8"> 
+     <title>Домашнее задание № 1</title>
+  </head>
+  <body>
+<?php
+$name = 'Максим';
+$age = 29;
+$email = 'maaax128@list.ru';
+$city = 'Пермь';
+$about = 'студент Нетологии';
+
+echo '<h1>Страница пользователя Максим</h1>';
+echo "Имя $name <br>";
+echo "Возраст $age <br>";
+echo "Адрес электронной почты $email <br>";
+echo "Город $city <br>";
+echo "О себе $about";
+
+
+
+  ?>
+</body>
+</html>
